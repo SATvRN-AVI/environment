3d51187cfffd79e81c189cd7dbd39726dc58be8a
Walter <hudson.walterf@gmail.com>
2021-06-25 08:35:32 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,16 +1,21 @@
 peripheralList = peripheral.getNames() 
 
+shell.setAlias("Main Menu", "startup")
+os.pullEvent("key")
+textutils.slowPrint("Booting SATvRN Core, standby...")
+os.sleep(1)
 print("Press any key to continue...")
 
-if next(peripheralList) then
-    local event = os.pullEvent("char")  
-    local id = multishell.launch({}, "rom/programs/")
+if next(peripheralList) then 
+    
     for i = 1, #peripheralList do
         print("There is a "..peripheral.getType(peripheralList[i]).." connected to the \""..peripheralList[i].."\"")
         if peripheral.getType(peripheralList[i]) == "modem" then
-            textutils.slowPrint("Modem found! Accessing modem...")
+            os.sleep(1)
+            textutils.slowPrint("Modem found!")
             os.sleep(2)
-            io.write("To power up modem for remote use, press Y, otherwise press N: ")
+            print("For a list of commands type \""corelist"\"") 
+            io.write("Awaiting command: ")
             local userinput = io.read()
             if userinput == "y" then
                 rednet.open(peripheralList[i])
@@ -19,16 +24,19 @@ if next(peripheralList) then
                 local id = multishell.launch({}, "/customboot/manualwireless")
                 multishell.setTitle(id, "Manual Control")
                 shell.switchTab(2)
-                return
-                shell.exit()
             elseif userinput == "n" then
                 textutils.slowPrint("Returning to root...")
                 return
+            
             else
                 print("This key is not recognized, please hit Y or N...")
+            
             end
+        
         end
+    
     end
+
 else
     textutils.slowPrint("Could not find any peripherals. Terminating bootup...")
     return
